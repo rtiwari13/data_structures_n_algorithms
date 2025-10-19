158b2407ea89587d12e24d0a13a557411a1baa16
rtiwari13 <ritanshutiwari593@gmail.com>
2025-10-20 00:41:13 +0530

    day :2

--- recursion/rec_6.py
+++ recursion/rec_6.py
@@ -1,5 +1,15 @@
+
 # Reverse an array
 
+def reverse_array(arr,left,right):
+    
+    if  right<left:
+        return arr
+    
+    arr[left],arr[right] = arr[right],arr[left]
 
+    return reverse_array(arr,left+1,right-1)
+     
+result = reverse_array([1,2,3,4,5],0,4)
+print(result)
 
-# check palindrome 
